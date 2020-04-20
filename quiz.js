8a13424d0e7bd9e82a2200436b54ce38a68d865a
Forrest Smith <62183752+forrestsmith94@users.noreply.github.com>
2020-04-19 21:09:21 -0600

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,193 +1,94 @@
-document.getElementById("submit").addEventListener("click", displaymyQuestions);
+// 
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
 
-<form id="form1">
-var myQuestions = [
-    {
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        // create 8 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
     }
-    {
-        // create 7 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
     }
-    {
-        // create 6 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
     }
-    {
-        // create 5 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
     }
-    {
-        // create 4 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
-    }
-    {
-        // create 3 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
-    }
-    {
-        // create 2 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
-    }
-    {
-        // create 1 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
-    }
-    {
-        // create 0 more questions
-        question: "What does Console.log do?",
-        answers: {
-            a: 'x',
-            b: 'x2',
-            c: 'x3'
-        },
-        correctAnswer: 'b'
-        
-    } </form>
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+//begin timer 
+
+// define variables as questions
+var questions = [
+    new Question("What is a form used for in JavaScript?", ["Submitting buttons", "Grouping and sending information to the server","As a way to console.log", "As a way to anchor links"], "Grouping and sending information to the server"),
+    new Question("What are variables?", ["Undetermined prompts", "A way to communicate with an HTML page", "A type of fruit", "The nouns of programming"], "The nouns of programming"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("What is the Console.log method used for?", ["Consoling logs", "Using JQueries", "An if/else statement", "Displaying data in a console"], "Displaying data in a console")
 ];
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+// define quiz
+var quiz = new Quiz(questions);
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-    function showQuestions(questions, quizContainer){
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
-
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-            
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for(letter in questions[i].answers){
-
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
-                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-                        + letter + ': '
-                        + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-
-        // finally combine our output list into one string of html and put it on the page
-        quizContainer.innerHTML = output.join('');
-    }
-
-
-    function showResults(questions, quizContainer, resultsContainer){
-        
-        // gather answer containers from our quiz
-        var answerContainers = quizContainer.querySelectorAll('.answers');
-        
-        // keep track of user's answers
-        var userAnswer = '';
-        var numCorrect = 0;
-        
-        // for each question...
-        for(var i=0; i<questions.length; i++){
-
-            // find selected answer
-            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-            
-            // if answer is correct
-            if(userAnswer===questions[i].correctAnswer){
-                // add to the number of correct answers
-                numCorrect++;
-                
-                // color the answers green
-                answerContainers[i].style.color = 'lightgreen';
-            }
-            // if answer is wrong or blank
-            else{
-                // color the answers red
-                answerContainers[i].style.color = 'red';
-            }
-        }
-
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
-
-    // show questions right away
-    showQuestions(questions, quizContainer);
-    
-    // on submit, show results
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-
-}
+// display quiz
+populate();
