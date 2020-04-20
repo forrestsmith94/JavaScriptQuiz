4e8c472e1d633231a30462090f98b22967810dd9
Forrest Smith <62183752+forrestsmith94@users.noreply.github.com>
2020-04-19 19:52:16 -0600

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,193 @@
+document.getElementById("submit").addEventListener("click", displaymyQuestions);
+
+<form id="form1">
+var myQuestions = [
+    {
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        // create 8 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 7 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 6 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 5 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 4 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 3 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 2 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 1 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+    }
+    {
+        // create 0 more questions
+        question: "What does Console.log do?",
+        answers: {
+            a: 'x',
+            b: 'x2',
+            c: 'x3'
+        },
+        correctAnswer: 'b'
+        
+    } </form>
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            // for each available answer...
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+
+}
